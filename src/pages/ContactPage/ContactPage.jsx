cb7bcc3a2e457fb0c1888f81b7c342df7d79427d
Jyotirmoy <jyotirmoydas12@gmail.com>
2025-07-20 14:37:04 +0530

    update contact page map link

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -39,7 +39,7 @@ const ContactPage = () => {
             <div className="map-embed">
               <iframe
                 title="DLSD Society Location"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224345.83920875844!2d77.06889921808932!3d28.527582000000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce3b6fdf2e44b%3A0xc2d5e9c1a5b5b9d5!2sNIIT%20Foundation!5e0!3m2!1sen!2sin!4v1718107050666!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3582.3139925382034!2d91.79053440970793!3d26.121303277032332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375a5f6d31adb0cb%3A0x3e7dd44ac638f90!2sDigital%20Literacy%20and%20Skill%20Development%20Society!5e0!3m2!1sen!2sin!4v1753002342753!5m2!1sen!2sin"
                 allowFullScreen
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
