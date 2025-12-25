78d79b2b955b523ae0cd0121e0b0f09438107dca
Jyotirmoy <jyotirmoydas12@gmail.com>
2025-12-26 01:16:57 +0530

    fix signup admin url

--- src/admin/AdminSignupPage.jsx
+++ src/admin/AdminSignupPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import BASE_URL from '../config/config';
 
 const AdminSignupPage = () => {
   const [form, setForm] = useState({
