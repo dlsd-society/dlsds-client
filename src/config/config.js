4d912d2571ef8b32b11dc05c23c6e179332308e0
Jyotirmoy <jyotirmoydas12@gmail.com>
2026-05-18 20:42:05 +0530

    change base_url

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 // const BASE_URL = 'http://localhost:5000/api';
 // const BASE_URL = "https://dlsds-server.onrender.com/api";
 
-const BASE_URL = "https://dlsds-server.onrender.com/api";
+// const BASE_URL = "https://dlsds-server.onrender.com/api";
 
-// const BASE_URL = "https://dlsdsociety.org/api";
+const BASE_URL = "https://dlsdsociety.org";
 
 export default BASE_URL;
